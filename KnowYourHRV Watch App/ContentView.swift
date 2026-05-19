0ee9b341786fb2d609d17ac7f503237fa06a3530
Jimmy Lu <jimmy.lu.2011@gmail.com>
2026-05-19 15:55:13 -0700

    Initial Commit

---
+++ KnowYourHRV Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KnowYourHRV Watch App
+//
+//  Created by Jimmy Lu on 5/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KnowYourHRV Watch App/KnowYourHRVApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowYourHRVApp.swift
+//  KnowYourHRV Watch App
+//
+//  Created by Jimmy Lu on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct KnowYourHRV_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
